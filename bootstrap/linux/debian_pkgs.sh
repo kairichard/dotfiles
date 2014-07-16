bb73071f17db1ee8f0e756c7ada8710c565e84e1
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-07-16 13:45:42 +0200

    Add ack-grep to the packages to be installed

--- bootstrap/linux/debian_pkgs.sh
+++ bootstrap/linux/debian_pkgs.sh
@@ -3,6 +3,7 @@ sudo apt-get purge vim
 sudo apt-get install mercurial build-essential ncurses-dev
 sudo apt-get install python-dev
 sudo apt-get install build-essential cmake
+sudo apt-get install ack-grep
 
 cd /tmp
 hg clone https://vim.googlecode.com/hg/ vim
