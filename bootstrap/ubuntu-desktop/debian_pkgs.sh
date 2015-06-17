d6e75cf98bb7ec87042f99e217b728bdf1bfc3d4
Kai Richard König <kai.richard.koenig@googlemail.com>
2015-06-17 17:14:22 +0200

    Don't ask for permission

--- bootstrap/ubuntu-desktop/debian_pkgs.sh
+++ bootstrap/ubuntu-desktop/debian_pkgs.sh
@@ -1,4 +1,3 @@
 sudo add-apt-repository ppa:klaus-vormweg/awesome
-sudo apt-get install awesome
-sudo apt-get install awesome-extra
-sudo apt-get install i3lock scrot imagemagick xautolock
+sudo apt-get install --yes awesome awesome-extra
+sudo apt-get install --yes i3lock scrot imagemagick xautolock
