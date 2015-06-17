4a2af981535f124633572840ae67b4bc0ffacd3c
Kai Richard König <kai.richard.koenig@googlemail.com>
2015-06-17 18:02:04 +0200

    confirm everything

--- bootstrap/ubuntu-desktop/debian_pkgs.sh
+++ bootstrap/ubuntu-desktop/debian_pkgs.sh
@@ -1,3 +1,3 @@
-sudo add-apt-repository ppa:klaus-vormweg/awesome
+sudo add-apt-repository --yes ppa:klaus-vormweg/awesome
 sudo apt-get install --yes awesome awesome-extra
 sudo apt-get install --yes i3lock scrot imagemagick xautolock
