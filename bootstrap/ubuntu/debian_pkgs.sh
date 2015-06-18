55e1be5a6a51a6a62e8d2addde3f49a26b019e48
Kai Richard König <kai.richard.koenig@googlemail.com>
2015-06-18 09:58:35 +0200

    Don't build vim from scratch

--- bootstrap/ubuntu/debian_pkgs.sh
+++ bootstrap/ubuntu/debian_pkgs.sh
@@ -2,15 +2,7 @@ echo 'Building VIM7.4'
 sudo apt-get purge --yes vim
 sudo apt-get install --yes mercurial ncurses-dev python-dev
 sudo apt-get install --yes build-essential cmake ack-grep exuberant-ctags
-sudo apt-get install --yes htop
-
-cd /tmp
-hg clone https://vim.googlecode.com/hg/ vim
-cd vim/src
-make distclean
-./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp
-make
-sudo make install
+sudo apt-get install --yes htop vim
 
 echo 'Installing rbenv'
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
