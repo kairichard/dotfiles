0047a55eee1769bdd2faeef05d27501cd2f0250c
Kai Richard König <kai.richard.koenig@googlemail.com>
2015-08-10 17:58:24 +0200

    Install tmux

--- bootstrap/ubuntu/debian_pkgs.sh
+++ bootstrap/ubuntu/debian_pkgs.sh
@@ -2,7 +2,7 @@ echo 'Building VIM7.4'
 sudo apt-get purge --yes vim
 sudo apt-get install --yes mercurial ncurses-dev python-dev
 sudo apt-get install --yes build-essential cmake ack-grep exuberant-ctags
-sudo apt-get install --yes htop vim
+sudo apt-get install --yes htop vim tmux
 
 echo 'Installing rbenv'
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
