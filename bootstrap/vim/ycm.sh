ddc665478a6fc7b4aef348d4f9d03bf531f61995
Kai Richard Koenig <kai.koenig@paessler.com>
2016-02-22 14:51:18 +0100

    No need to install completer that no one uses

--- bootstrap/vim/ycm.sh
+++ bootstrap/vim/ycm.sh
@@ -1,6 +1,5 @@
 echo "Building YCM"
 cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --omnisharp-completer --gocode-completer \
-      --tern-completer --racer-completer
+./install.py --clang-completer
 echo "DONE"
 
