e329c2d6f2b05161cf129c46235e9962778557a7
Kai Richard König <kai.richard.koenig@gmail.com>
2024-12-04 19:43:27 +0100

    Add zsh support

---
+++ spaceshiprc.zsh
@@ -0,0 +1,45 @@
+SPACESHIP_PROMPT_PREFIXES_SHOW=false
+
+SPACESHIP_PROMPT_ORDER=(
+  time          # Time stampts section
+  user          # Username section
+  host          # Hostname section
+  dir           # Current directory section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  package       # Package version
+  node          # Node.js section
+  ruby          # Ruby section
+  elixir        # Elixir section
+  xcode         # Xcode section
+  swift         # Swift section
+  golang        # Go section
+  php           # PHP section
+  rust          # Rust section
+  haskell       # Haskell Stack section
+  julia         # Julia section
+  docker        # Docker section
+  aws           # Amazon Web Services section
+  venv          # virtualenv section
+  conda         # conda virtualenv section
+  # pyenv         # Pyenv section
+  dotnet        # .NET section
+  # ember         # Ember.js section
+  exec_time     # Execution time
+  line_sep      # Line break
+  battery       # Battery level and status
+  # vi_mode       # Vi-mode indicator
+  jobs          # Backgound jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+) 
+  
+SPACESHIP_USER_SHOW="always" # Shows System user name before directory name
+ 
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+# SPACESHIP_PROMPT_SEPARATE_LINE=false # Make the prompt span across two lines
+# SPACESHIP_DIR_TRUNC=1 # Shows only the last directory folder name
+ 
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+SPACESHIP_PYTHON_SHOW=true
---
+++ zshenv
@@ -0,0 +1,1 @@
+PLATTFROM=$(uname|tr [:upper:] [:lower:])
